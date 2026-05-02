b27db1ec88a08a6b3411bd25efe072bba161d7fa
kribo852 <kristoffer.a.borg@gmail.com>
2026-05-02 11:28:38 +0200

    Added a new dithering algorithm
    The new algorithm uses a pseudo random number for deciding if the pixel
    is on or off. But also, contrasts with neighbouring pixels decide what constants
    to use when generating the pseudo random number.
    
    My idea is that one will see that changes in contrast, even if the picture remains
    the same brightness.
    
    This algorithm is not good at keeping the natural brightnesses, is also
    produces artefacts in the resulting image, but the idea is that the details will
    be clearer.
    
    Also added a new picture to test the algorithm on.

---
+++ pseurandom_with_contrast.lua
@@ -0,0 +1,111 @@
+pseudorandom = {}
+
+local function mod(a, b)
+    return a - (math.floor(a/b)*b)
+end
+
+local function get_min_rgb()
+    if not pseudorandom.min_r then
+        pseudorandom.min_r = 1
+        pseudorandom.min_g = 1
+        pseudorandom.min_b = 1
+
+        for x = 0,pseudorandom.get_scaled_width() - 1 do
+            for y = 0,pseudorandom.get_scaled_height() - 1 do
+                local tmp_r, tmp_g, tmp_b = pseudorandom.measure_color(x, y)
+                pseudorandom.min_r = math.min(pseudorandom.min_r, tmp_r)
+                pseudorandom.min_g = math.min(pseudorandom.min_g, tmp_g)
+                pseudorandom.min_b = math.min(pseudorandom.min_b, tmp_b)
+            end
+        end
+    end
+
+    return pseudorandom.min_r, pseudorandom.min_g, pseudorandom.min_b
+end
+
+local function get_max_rgb()
+    if not pseudorandom.max_r then
+        pseudorandom.max_r = 0
+        pseudorandom.max_g = 0
+        pseudorandom.max_b = 0
+
+        for x = 0,pseudorandom.get_scaled_width() - 1 do
+            for y = 0,pseudorandom.get_scaled_height() - 1 do
+                local tmp_r, tmp_g, tmp_b = pseudorandom.measure_color(x, y)
+                pseudorandom.max_r = math.max(pseudorandom.max_r, tmp_r)
+                pseudorandom.max_g = math.max(pseudorandom.max_g, tmp_g)
+                pseudorandom.max_b = math.max(pseudorandom.max_b, tmp_b)
+            end
+        end
+    end
+    return pseudorandom.max_r, pseudorandom.max_g, pseudorandom.max_b
+end
+
+local function vector_length(r, g, b)
+    return math.sqrt(r^2 + g^2 + b^2)
+end
+
+local function calc_x_contrast(x, y)
+    local contrast = 0
+    local tmp_r, tmp_g, tmp_b = pseudorandom.measure_color(x, y)
+
+    if x-1 >= 0 then
+        local tmp_r_minus, tmp_g_minus, tmp_b_minus = pseudorandom.measure_color(x-1, y)
+        contrast = math.abs( tmp_r - tmp_r_minus ) + math.abs( tmp_g - tmp_g_minus ) + math.abs( tmp_b + tmp_b_minus )
+    end
+
+    if x+1 < pseudorandom.get_scaled_width() then
+        local tmp_r_plus, tmp_g_plus, tmp_b_plus = pseudorandom.measure_color(x+1, y)
+        contrast = contrast + math.abs( tmp_r - tmp_r_plus ) + math.abs( tmp_g - tmp_g_plus ) + math.abs( tmp_b + tmp_b_plus )
+    end
+
+    return contrast
+end
+
+local function calc_y_contrast(x, y)
+    local contrast = 0
+    local tmp_r, tmp_g, tmp_b = pseudorandom.measure_color(x, y)
+
+    if y-1 >= 0 then
+        local tmp_r_minus, tmp_g_minus, tmp_b_minus = pseudorandom.measure_color(x, y-1)
+        contrast = math.abs( tmp_r - tmp_r_minus ) + math.abs( tmp_g - tmp_g_minus ) + math.abs( tmp_b + tmp_b_minus )
+    end
+
+    if y+1 < pseudorandom.get_scaled_height() then
+        local tmp_r_plus, tmp_g_plus, tmp_b_plus = pseudorandom.measure_color(x, y+1)
+        contrast = contrast + math.abs( tmp_r - tmp_r_plus ) + math.abs( tmp_g - tmp_g_plus ) + math.abs( tmp_b + tmp_b_plus )
+    end
+
+    return contrast
+end
+
+function pseudorandom.mapping_algorithm(x, y, r, g, b, a)
+
+    --get min values rgb, calculate max rgb values, do this in a singleton
+    local min_r, min_g, min_b = get_min_rgb()
+    local max_r, max_g, max_b = get_max_rgb()
+    
+    --calculate constants for the random function, 2-4 different sets depending on how much contrast/frequency there is
+    local x_rng_constant = 21
+    local y_rng_constant = 23
+
+    if calc_x_contrast(x,y) > 0.1 then
+        x_rng_constant = 47
+    end
+
+    if calc_y_contrast(x,y) > 0.1 then
+        y_rng_constant = 51
+    end 
+
+    local pseudo_random_number = mod((x_rng_constant*x + y_rng_constant*y), 301)/301
+
+    --normalize the measured value 0-1 ?
+    if pseudo_random_number*vector_length(max_r-min_r, max_g-min_g, max_b-min_b) < 
+        vector_length(r-min_r, g-min_g, b-min_b) then
+    	return 1.0, 1.0, 1.0, a
+    end
+
+    return 0, 0, 0, a
+end
+
+return pseudorandom
